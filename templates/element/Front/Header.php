59de33c4a83c1a66e9af446f856dfac95cc573f7
VAITON DAMIEN p2106425 <damien.vaiton@etu.univ-lyon1.fr>
2023-03-01 21:30:49 +0100

    Test

--- templates/element/Front/Header.php
+++ templates/element/Front/Header.php
@@ -4,7 +4,7 @@
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png" alt="logo" width="20%">
 
         </div>
-        $
+
         <div class="card-image right-align ">
             <img src="https://www.pngall.com/wp-content/uploads/5/Shopping-Cart.png" alt="logo" width="20%">
     </div>
