028247ebddbdb8f5d3621834b0d1a8257e3f041f
VAITON DAMIEN p2106425 <damien.vaiton@etu.univ-lyon1.fr>
2023-03-01 12:30:03 +0100

    Debut graphique

--- templates/element/Front/Footer.php
+++ templates/element/Front/Footer.php
@@ -2,10 +2,11 @@
 
 
 
-    <p>Le site de fou</p>
-    <div class="card-image right-align">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png" alt="logo" width="15%">
+    <div class="card-image">
+    <div class="card-image right-align ">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png" alt="logo" width="5%">
     </div>
+</div>
 
 
 
