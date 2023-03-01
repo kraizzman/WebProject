d9e074c7686fe3f9aad8995ef7f0833e46660603
VAITON DAMIEN p2106425 <damien.vaiton@etu.univ-lyon1.fr>
2023-03-01 21:47:38 +0100

    Debut page login ( par default va sur login)

--- templates/Pages/Login.php
+++ templates/Pages/Login.php
@@ -1,23 +1,29 @@
-<section class="yellow">
+<section class="container">
 
 
-    <div class="col s12">
+
+
+    <div class="col m12">
         <?php echo $this->Form->create(null, ['type' => 'post', 'class' => "form-horizontal style-form"]); ?>
-        <div class="input-field col s10">
+        <div class="col m8">
             <label class="col-sm-2 col-sm-2 control-label">Username</label>
-            <div class="col-sm-10">
+            <div>
                 <?php echo $this->Form->control('username', ['class' => 'form-control', 'label' => false]); ?>
             </div>
 
         </div>
-        <div class="input-field col s1">
+        <div class="col m8">
             <label class="col-sm-2 col-sm-2 control-label">Password</label>
-            <div class="col-sm-10">
+            <div>
                 <?php echo $this->Form->control('password', ['class' => 'form-control', 'label' => false]); ?>
             </div>
-            <div class="pull-l3">
-                <?php echo $this->Form->button('Enregistrer', ['type' => 'submit', 'class' => 'btn btn-primary']); ?>
+        </div>
+            <div class="center">
+                <?php echo $this->Form->button('Submit', ['type' => 'submit', 'class' => 'btn btn-primary']); ?>
 
             </div>
+
+        <?php echo $this->Form->end(); ?>
+    </div>
 
 </section>
