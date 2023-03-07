e9df4a0e2241535bbc3103aef0420ad1cf1890f5
VAITON DAMIEN p2106425 <damien.vaiton@etu.univ-lyon1.fr>
2023-03-07 21:26:59 +0100

    J'arrive pas a mettre mes element a droite

--- templates/element/Front/Header.php
+++ templates/element/Front/Header.php
@@ -1,43 +1,19 @@
-
-<header class="header container-fluid ">
-
-   <div class="d-flex bg-primary justify-content-between">
-
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png" class="col-sm-2  " height="20%">
-
-       <div class="d-flex flex-row-reverse">
-
-       <?php
-       echo $this->Html->image("logintest.png", [
-               "alt" => "Brownies",
-               'url' => ['controller' => 'Users', 'action' => 'Login', 6],
-               'class' => 'col-sm-1 '
-
-
-
-           ]
-       );
-
-
-
-
-
-
-
-
-       echo $this->Html->image("shopping-cart-logo.svg", [
-           "alt" => "Brownies",
-           'url' => ['controller' => 'Users', 'action' => 'Panier', 6],
-              'class' => 'col-sm-1 '
-
-
-
-       ]
-       );
-       ?>
-
-       </div>
-
-   </div>
-
+<header class="header">
+    <div class="d-flex bg-primary justify-content-between">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png" class="col-sm-2" height="20%">
+        <div class="d-flex justify-content-end col-sm-10">
+            <?php
+            echo $this->Html->image("logintest.png", [
+                "alt" => "Brownies",
+                'url' => ['controller' => 'Users', 'action' => 'Login', 6],
+                'class' => 'col-sm-1'
+            ]);
+            echo $this->Html->image("shopping-cart-logo.svg", [
+                "alt" => "Brownies",
+                'url' => ['controller' => 'Users', 'action' => 'Panier', 6],
+                'class' => 'col-sm-1'
+            ]);
+            ?>
+        </div>
+    </div>
 </header>
